bcd6e2d2ba242067afacb74fc2e07b3f3972fbcc
ZAHEER0011 <43technetium99.collab@gmail.com>
2026-02-27 14:50:02 +0530

    CryptoLytics in Production

--- src/data/api.js
+++ src/data/api.js
@@ -68,7 +68,8 @@
 
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+// const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://cryptolytics-backend.onrender.com/api";
 
 export const getMarketData = async (
   currency,
